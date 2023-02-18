c3aef9228680eaf506e4ebe84eda7d81eec7656e
Sstark97 <aitorscinfo@gmail.com>
2023-02-18 11:04:25 +0000

    Finish Laravel-3

--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 @section('content')
     <h1>Community</h1>
+    @foreach ($links as $link)
+        <li>{{$link->title}}</li>
+        <small class="mb-3">Contributed by: {{$link->creator->name}} {{$link->updated_at->diffForHumans()}}</small>
+    @endforeach
+    {{$links->links()}}
 @stop
